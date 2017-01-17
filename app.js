0f1e41a9a06f5d5dc69e2c34039154ee985f8a81
Luisa de Miranda <lemiranda@gmail.com>
2017-01-17 12:09:32 -0500

    many templates an routingadded

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require( 'express');
-const nunjucks = require('nunjucks');
 const app = express();
+const nunjucks = require('nunjucks');
+const routes = require('./routes/');
 
+app.use('/', routes);
 
 app.use(function (req, res, next){ //need this first
     console.log('Request:', req.method, req.path); //find status-getter
@@ -13,13 +15,13 @@ app.use('/special/', function (req, res, next){ //middleware- anything starting 
     //res.send('you reached the special area');
     next();
 })
-app.get('/', function(req,res) {
-    res.render('index.html', locals);
-})
-
-app.get('/news', function(req,res) {
-    res.send('Hello news')
-})
+// app.get('/', function(req,res) {
+//     res.render('index.html', locals);
+// })
+//
+// app.get('/news', function(req,res) {
+//     res.send('Hello news')
+// })
 
 app.listen(3000, function(){
     console.log('server listening')
@@ -44,4 +46,3 @@ var locals = {
 nunjucks.render('index.html', locals, function (err, output) {
     console.log(output);
 });
-
---
+++ routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const tweetBank = require('../tweetBank');
+
+router.get('/', function (req, res) {
+  let tweets = tweetBank.list();
+  res.render( 'index', { tweets: tweets } );
+});
+
+router.use(express.static('public'))
+
+module.exports = router;
